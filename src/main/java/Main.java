d47107322af9c56afaa11b70167601ce7b2b9086
Aaron Dbritto <143990364+Adbritto@users.noreply.github.com>
2025-06-23 17:57:55 -0700

    type builtin

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,13 +7,38 @@ public class Main {
         while(true) {
             System.out.print("$ ");
             String input = scanner.nextLine();
-            if (input.trim().equals("exit 0")) {
-                break;
-            } else if (input.trim().contains("echo")) {
-                System.out.println(input.substring(5));
-            } else {
-                System.out.println(input + ": command not found");
+            String[] commands = input.split(" ");
+
+            switch (commands[0]) {
+                case "exit" -> System.exit(Integer.parseInt(commands[1]));
+                case "type" -> type(commands);
+                case "echo" -> echo(input);
+                default -> printCNF(commands[0]);
             }
         }
+    }
+
+    static void echo(String input) {
+        System.out.println(input.substring(5));
+    }
+
+    static void printCNF(String input) {
+        System.out.println(input + ": command not found");
+    }
+
+    static void type(String[] input) {
+        String[] validCmds = {"exit", "type", "echo"};
+        if (search(validCmds, input[1])) {
+            System.out.println(input[1] + "is a shell builtin");
+        } else {
+            System.out.println(input[1] + ": not found");
+        }
+    }
+
+    static boolean search(String[] input, String command) {
+        for (String s: input) {
+            if (s.equals(command)) return true;
+        }
+        return false;
     }
 }
