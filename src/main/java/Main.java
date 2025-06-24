5615b3e26a869952d968650379a8b09d840104e3
Aaron Dbritto <143990364+Adbritto@users.noreply.github.com>
2025-06-23 17:24:50 -0700

    exit builtin

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,15 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // Uncomment this block to pass the first stage
-         System.out.print("$ ");
-
         Scanner scanner = new Scanner(System.in);
 
         while(scanner.hasNextLine()) {
-            String input = scanner.nextLine();
-            System.out.println(input + ": command not found");
             System.out.print("$ ");
+            String input = scanner.nextLine();
+            if (input.trim().equals("exit 0")) break;
+            System.out.println(input + ": command not found");
         }
     }
 }
